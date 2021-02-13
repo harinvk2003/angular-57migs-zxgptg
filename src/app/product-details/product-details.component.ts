cc560fc746f7a465b7e53f8745cfc36da658e65a
harinvk2003 <harinvk2003@gmail.com>
2021-02-13 10:25:25 -0800

    4 files modified, 7 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { products } from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product-details",
@@ -9,9 +10,10 @@ import { products } from "../products";
 })
 export class ProductDetailsComponent implements OnInit {
   product: any;
+  adddedProduct: any;
 
   //injecting the activated route into constrcutor to use it as a service within the component. this is similar to using services
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private cartservice: CartService) {
     //get the productId from the current route link
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get("productId"));
@@ -23,4 +25,14 @@ export class ProductDetailsComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  addToCart(product) {
+    //check if the product already exists in the cart
+    if (this.cartservice.items.includes(product)) {
+      window.alert("Your product already exists in the cart!");
+    } else {
+      this.cartservice.addToCart(product);
+      window.alert("Your product has been added to the cart!");
+    }
+  }
 }
